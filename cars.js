4d55c264c17d692ba668a3b2e978de69fac9244e
Jha-Ujjwal-1542 <ujjwal.jha@apexcollege.edu.np>
2023-03-15 16:30:56 +0545

    test completed

---
+++ cars.js
@@ -0,0 +1,72 @@
+const cars = [
+   {
+     title: "Rolls Royce Phantom",
+     date: "2017-03-12",
+     image: "./assets/P90250378_highRes_rolls-royce-ghost-ex-680x454.jpg",
+     author: "John Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Ioniq 5",
+     date: "2017-02-12",
+     image: "https://i.gaw.to/content/photos/50/43/504357-hyundai-ioniq-5-2022-serieusement-convaincante.jpg",
+     author: "Sankita Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Range Rover",
+     date: "2017-02-12",
+     image: "https://media.ed.edmunds-media.com/land-rover/range-rover/2022/oem/2022_land-rover_range-rover_4dr-suv_p530-autobiography-lwb-5_fq_oem_1_1600.jpg",
+     author: "Mia Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Bentley Bentayga",
+     date: "2017-02-12",
+     image: "./assets/bentley.png",
+     author: "Johnny Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "BMW i20 SLS",
+     date: "2017-02-12",
+     image: "./assets/bmw.jpg",
+     author: "Jane Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Koineggsegg Agera R",
+     date: "2017-02-12",
+     image: "./assets/koinigsegg agera r.PNG",
+     author: "Jay Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Mini Cooper",
+     date: "2017-02-12",
+     image: "./assets/mini cooper.PNG",
+     author: "John Doe",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Mercedes AMG S",
+     date: "2017-02-12",
+     image: "./assets/Mercedes-AMG-GT-Concept-10-952x563.jpg",
+     author: "Ujjwal Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Toyota Land Cruiser",
+     date: "2017-02-12",
+     image: "./assets/Toyota Land Cruiser 2021 upgraded.png",
+     author: "Harry Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   },
+   {
+     title: "Ford Bullitt",
+     date: "2011-02-12",
+     image: "./assets/bullitt.png",
+     author: "Jaden Smith",
+     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Nulla aliquet enim tortor at auctor."
+   }
+];
